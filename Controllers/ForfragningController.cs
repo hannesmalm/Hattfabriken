d76e15c313e3f9ca259274b12577efc8071257d1
filipnyden <132461429+filipnyden@users.noreply.github.com>
2024-04-03 13:54:29 +0200

    Controller och Vy

---
+++ Controllers/ForfragningController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Hattfabriken.Controllers
+{
+    public class ForfragningController : Controller
+    {
+        public IActionResult ForFragning()
+        {
+            return View();
+        }
+    }
+}
