c05e2a64cbc8ab4ffeb2c98cc7abe5297281aa4b
hannesmalm <128385053+hannesmalm@users.noreply.github.com>
2024-04-03 14:42:13 +0200

    + CreateAccount

--- Models/Viewmodels/CreateAccount.cs
+++ Models/Viewmodels/CreateAccount.cs
@@ -1,9 +1,22 @@
-﻿
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
 
 
 namespace Hattfabriken.Models.Viewmodels
 {
-    public class CreateAccount
+    public class CreateAccount : IdentityUser
     {
+        [Required(ErrorMessage = "Vänligen välj ett användarnamn")]
+        [Display(Name = "Användarnamn")]
+        [MinLength(3, ErrorMessage = "Användarnamnet måste vara minst 3 tecken långt.")]
+        [Key]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Vänligen fyll i ett lösenord")]
+        [Display(Name = "Lösenord")]
+        [MinLength(6, ErrorMessage = "Lösenordet måste vara minst 6 tecken långt.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
     }
 }
