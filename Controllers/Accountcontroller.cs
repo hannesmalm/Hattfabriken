10f1e79b6d6a20a52b89ad14be8838bb71778b48
kanikaradaghi <kani_karadaghi@hotmail.com>
2024-04-03 13:48:00 +0200

    ändring

---
+++ Controllers/Accountcontroller.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Hattfabriken.Controllers
+{
+    public class Accountcontroller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
