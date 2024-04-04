31a8d565c83ff88cd661796c816640bb521a261a
MaximilianWik <132461608+MaximilianWik@users.noreply.github.com>
2024-04-04 16:00:51 +0200

    Warehouse le epic meow

---
+++ Controllers/MaterialController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using Hattfabriken.Models;
+
+namespace Hattfabriken.Controllers
+{
+    public class MaterialController : Controller
+    {
+        private readonly HatDbContext _dbContext;
+
+        public MaterialController(HatDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            var materials = _dbContext.Materials.ToList();
+            return View(materials);
+        }
+
+        public IActionResult StorageOfMaterials()
+        {
+            var materials = _dbContext.Materials.ToList();
+            return View("~/Views/Lager/StorageOfMaterials.cshtml", materials);
+        }
+
+
+        [HttpPost]
+        public IActionResult AddMaterial(Material material)
+        {
+            if (ModelState.IsValid)
+            {
+                _dbContext.Materials.Add(material);
+                _dbContext.SaveChanges();
+                return RedirectToAction(nameof(StorageOfMaterials));
+            }
+            return View(material);
+        }
+
+        public IActionResult EditMaterial(string id)
+        {
+            var material = _dbContext.Materials.FirstOrDefault(m => m.MaterialName == id);
+            if (material == null)
+            {
+                return NotFound();
+            }
+            return View("~/Views/Lager/EditMaterial.cshtml", material);
+        }
+
+        [HttpPost]
+        public IActionResult EditMaterial(Material material)
+        {
+            if (ModelState.IsValid)
+            {
+                _dbContext.Materials.Update(material);
+                _dbContext.SaveChanges();
+                return RedirectToAction(nameof(StorageOfMaterials));
+            }
+            return View(material);
+        }
+    }
+}
