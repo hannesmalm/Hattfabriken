1ee36e5fbfbb24c1fbb15e29b3dd5ddc54af6fcb
filipnyden <132461429+filipnyden@users.noreply.github.com>
2024-04-04 14:52:10 +0200

    Alla fält fix

--- Models/Forfragan.cs
+++ Models/Forfragan.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Security.Permissions;
 
 namespace Hattfabriken.Models
 {
@@ -15,8 +16,13 @@ namespace Hattfabriken.Models
         public int Hojd { get; set; }
         public string Kommentar { get; set; }
         public string SpecialEffekter { get; set; }
+        public string Adress { get; set; }
+        public int Postnummer { get; set; }
+        public int Telefonnummer { get; set; }
+        public string Land { get; set; }
         [ForeignKey("Customer")]
         public string Email { get; set; }
+        
 
     }
 }
