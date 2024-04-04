586924c20aa77be46bdc37676e699b755b082969
filipnyden <132461429+filipnyden@users.noreply.github.com>
2024-04-04 14:18:25 +0200

    Hattfix

--- Models/Forfragan.cs
+++ Models/Forfragan.cs
@@ -10,5 +10,13 @@ namespace Hattfabriken.Models
         [ForeignKey("Hatt")]
         public int HatId
         { get; set; }
+        public string Material { get; set; }
+        public int Matt { get; set; }
+        public int Hojd { get; set; }
+        public string Kommentar { get; set; }
+        public string SpecialEffekter { get; set; }
+        [ForeignKey("Customer")]
+        public string Email { get; set; }
+
     }
 }
