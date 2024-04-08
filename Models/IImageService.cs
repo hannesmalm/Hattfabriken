a211b15426c17053d819f574e7d19c371d94a4ae
Phizzledizzle <phialv231@edunet.oru.se>
2024-04-08 11:46:52 +0200

    IImageService-Interface samt Image-klass

---
+++ Models/IImageService.cs
@@ -0,0 +1,19 @@
+﻿namespace Hattfabriken.Models
+{
+    public interface IImageService
+    {
+        byte[] ConvertToByteArray(IFormFile file);
+    }
+
+    public class ImageService : IImageService
+    {
+        public byte[] ConvertToByteArray(IFormFile file)
+        {
+            using (MemoryStream memoryStream = new MemoryStream())
+            {
+                file.CopyTo(memoryStream);
+                return memoryStream.ToArray();
+            }
+        }
+    }
+}
---
+++ Models/Image.cs
@@ -0,0 +1,8 @@
+﻿namespace Hattfabriken.Models
+{
+    public class Image
+    {
+        public int Id { get; set; }
+        public byte[] Data { get; set; }
+    }
+}
