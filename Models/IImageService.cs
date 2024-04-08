14910df9ba3789a1b3730b2e4fbfa3d053a92e14
Phizzledizzle <phialv231@edunet.oru.se>
2024-04-08 10:43:41 +0200

    Interface för bilder samt Image-klass

---
+++ Models/IImageService.cs
@@ -0,0 +1,19 @@
+﻿namespace Hattfabriken.Models
+{
+    public interface IImageService
+    {
+        byte[] ConvertToByteArray(IFormFile file);
+    }
+
+    public class ImageService : IImageService
+    {
+        public byte[] ConvertToByteArray(IFormFile file)
+        {
+            using (MemoryStream memorystream = new MemoryStream())
+            {
+                file.CopyTo(memorystream);
+                return memorystream.ToArray();
+            }
+        }
+    }
+}
---
+++ Models/Image.cs
@@ -0,0 +1,8 @@
+﻿namespace Hattfabriken.Models
+{
+    public class Image
+    {
+        public int ID { get; set; }
+        public Byte[] Data { get; set; }
+    }
+}
