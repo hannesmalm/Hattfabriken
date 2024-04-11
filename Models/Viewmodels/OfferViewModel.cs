3bf49b31938cee432fac35241b74d4976355ad3a
kanikaradaghi <kani_karadaghi@hotmail.com>
2024-04-11 14:08:40 +0200

    + ny model för offert och ny view för offert formulär

---
+++ Models/Viewmodels/OfferViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+
+namespace Hattfabriken.Models.Viewmodels
+{
+    public class OfferViewModel
+    {
+
+        public string? KundNamn { get; set; }
+
+        public string? KundMail {  get; set; }
+
+        public int OffertId { get; set; }
+
+        public decimal MaterialKostand {  get; set; }
+
+        public decimal SpecialeffektKostand { get; set; }
+
+        public decimal SpecialtygKostnad {  get; set; }
+
+        public decimal FraktKostnad { get; set; }
+
+        public DateTime EstimeratLeveransdatum { get; set; }
+
+
+    }
+}
+
+
