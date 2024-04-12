16ba4b5f6857b20bc1df72d6cf90a5763a9a0e77
lillyjuliat <testonglilly@gmail.com>
2024-04-12 13:41:54 +0200

    +viewmodel editHat

---
+++ Models/Viewmodels/EditHatViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Hattfabriken.Models
+{
+    public class EditHatViewModel
+    {
+        public int HatId { get; set; }
+        public string HatName { get; set; }
+        public string MaterialName { get; set; }
+        public string Description { get; set; }
+        public int Price { get; set; }
+        public string SpecialEffects { get; set; }
+        public int OuterMeasurement { get; set; }
+    }
+}
