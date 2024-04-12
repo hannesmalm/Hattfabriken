5e612aa5484908c08a8dbae266c3755bfcd0279c
kanikaradaghi <kani_karadaghi@hotmail.com>
2024-04-12 13:15:22 +0200

    ändringar

--- Models/Viewmodels/OfferViewModel.cs
+++ Models/Viewmodels/OfferViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 
 
@@ -9,19 +10,23 @@ namespace Hattfabriken.Models.Viewmodels
 
         public string? KundNamn { get; set; }
 
-        public string? KundMail {  get; set; }
+        public string? KundMail { get; set; }
 
         public int OffertId { get; set; }
 
-        public decimal MaterialKostand {  get; set; }
+        public decimal MaterialKostnad { get; set; }
 
-        public decimal SpecialeffektKostand { get; set; }
+        public decimal SpecialeffektKostnad { get; set; }
 
-        public decimal SpecialtygKostnad {  get; set; }
+        public decimal SpecialtygKostnad { get; set; }
 
         public decimal FraktKostnad { get; set; }
 
-        public DateTime EstimeratLeveransdatum { get; set; }
+
+        [Required(ErrorMessage = "Choose a date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+       public DateTime EstimeratLeveransdatum { get; set; }
 
 
     }
