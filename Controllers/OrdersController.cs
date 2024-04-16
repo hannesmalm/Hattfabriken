4238c34018d730f93cf78f5716a443a9c7b90e27
filipnyden <132461429+filipnyden@users.noreply.github.com>
2024-04-16 10:31:46 +0200

    Design för orderhanteringen

---
+++ Controllers/OrdersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Hattfabriken.Controllers
+{
+    public class OrdersController : Controller
+    {
+        public IActionResult Orders()
+        {
+            return View();
+        }
+    }
+}
